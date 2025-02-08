81b4e4d2486189df0be1b8fff095d8bcbcd3c778
ahmed emad <ahmed.emad.a.e.m@gmail.com>
2025-02-08 23:12:00 +0200

    Done

--- lib/device_model.dart
+++ lib/device_model.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter/material.dart';
 import 'dart:io' show Platform;
 
 class DeviceModel extends StatefulWidget {
